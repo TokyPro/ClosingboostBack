6a25239769193f52afe395d2d65663b56fb40b8c
Toky RAJAONA <rajaonatoky@gmail.com>
2026-04-25 15:10:00 +0300

    cors error

--- app/main.py
+++ app/main.py
@@ -58,6 +58,8 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://localhost:3001",
         "http://127.0.0.1:3001",
+        "http://35.205.217.120",
+        "http://35.205.217.120:3000",
     ],
     allow_credentials=True,
     allow_methods=["*"],
